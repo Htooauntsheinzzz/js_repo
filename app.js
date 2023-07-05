a47dd33b3ade6da8b72b2542068c196b0c65f981
Htooauntsheinzzz <htooauntsheinz@gmail.com>
2023-07-05 19:15:00 +0630

    For Ko si Thu

---
+++ app.js
@@ -0,0 +1,34 @@
+// conditional operator
+// == , === ,> ,<,>= ,<=, !=
+let x =20;
+let y ="20";
+
+let z= x  <=   y;
+
+console.log(z);
+
+
+// control flow
+
+let k =20;
+
+if(60>50){
+    console.log("Bigger");
+}else{
+    console.log("smaller");
+}
+
+
+// using switch
+
+let color="redy";
+
+switch (color){
+    case "green": console.log("It is green");break;
+    case "yellow":console.log("It is yellow");break;
+    case  "red": console.log("It is red");break;
+    default: console.log("Cannot Guess");
+}
+
+
+
